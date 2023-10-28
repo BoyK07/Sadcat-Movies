f7b92ebed3358f444bc601f65c47633d1fd3ed46
BoyK07 <boy.krijnen@ziggo.nl>
2023-10-29 00:01:47 +0200

    Images home page / scroll
    Images show up on home page and they are scrollable.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,18 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-2 sm:px-4 lg:px-6">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <div class="p-6 bg-white border-b border-gray-200 dark:bg-gray-800 dark:text-white">
+                    <h1 class="text-2xl font-bold mb-4">Popular</h1>
+                    <div class="carousel-container relative">
+                        <div class="owl-carousel">
+                            @foreach ($movies as $movie)
+                                <div class="item">
+                                    <img src="{{ $imageHelper->getUrl($movie->getPosterImage(), 'w400') }}" alt="Movie Poster" class="rounded-md shadow-lg">
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>                    
                 </div>
             </div>
         </div>
