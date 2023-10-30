e2f3de60084953edd724d7ecd9a7511df2c00ff2
BoyK07 <boy.krijnen@ziggo.nl>
2023-10-30 23:05:03 +0100

    Restyled search-bar

--- resources/views/search/result.blade.php
+++ resources/views/search/result.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
     <div class="p-5">
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="p-5 text-gray-900 dark:text-gray-100">
                 <form action="{{ route('search.search') }}" method="POST">
                     <div class="relative text-gray-600 dark:text-gray-400 ">
                         <input type="search" name="search" placeholder="Search" value="{{ request()->get('search') }}"
-                            class="bg-white dark:bg-gray-800 h-10 px-5 pr-10 rounded-full text-sm focus:outline-none w-full">
-                        <button type="submit" class="absolute right-0 top-0 mt-2 mr-4">
+                            class="bg-white dark:bg-gray-800 h-10 px-5 pr-10 py-6 rounded-full text-lg focus:outline-none w-full">
+                        <button type="submit" class="absolute right-0 top-0 mt-4 mr-6">
                             <i class="fa-solid fa-search"></i>
                         </button>
                     {{ csrf_field() }}
@@ -19,11 +19,11 @@
                             <div class="p-6 text-gray-900 dark:text-gray-100">
                                 @if ($result['media_type'] != "tv")
                                     <a href="{{ route('mv.info', $result['id']) }}">
-                                        <img src="{{ $imageHelper->getUrl($result['poster_path'], 'w200') }}" alt="image" class="rounded-md shadow-lg imageHover">
+                                        <img src="{{ $imageHelper->getUrl($result['poster_path'], 'w300') }}" alt="image" class="rounded-md shadow-lg imageHover">
                                     </a>
                                 @elseif ($result['media_type'] == "tv")
                                     <a href="{{ route('tv.info', $result['id']) }}">
-                                        <img src="{{ $imageHelper->getUrl($result['poster_path'], 'w200') }}" alt="image" class="rounded-md shadow-lg imageHover">
+                                        <img src="{{ $imageHelper->getUrl($result['poster_path'], 'w300') }}" alt="image" class="rounded-md shadow-lg imageHover">
                                     </a>
                                 @endif
                             </div>
