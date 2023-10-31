38e66b7244b0c17221e30f7ecb31e942c8af2fb1
BoyK07 <boy.krijnen@ziggo.nl>
2023-10-31 14:54:14 +0100

    Added watchlist

--- app/Http/Controllers/WatchlistController.php
+++ app/Http/Controllers/WatchlistController.php
@@ -8,11 +8,21 @@ use App\Models\Watchlist;
 
 class WatchlistController extends Controller
 {
+    public function show(){
+        $user = Auth::user();
+        $watchlist = Watchlist::where('user_id', $user->id)->get();
+        $watchlist = $watchlist->sortBy('title');
+        $watchlist = $watchlist->values()->all();
+        return view('watchlist.index', ['watchlist' => $watchlist]);
+    }
+
     public function add(Request $request) {
         $user = Auth::user();
         $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'type' => 'required|string',
+            'imageurl' => 'required|string',
+            'logo_url' => 'required|string',
             'tmdb_id' => 'required|integer',
         ]);
         
@@ -20,6 +30,8 @@ class WatchlistController extends Controller
             'user_id' => $user->id,
             'title' => $validatedData['title'],
             'type' => $validatedData['type'],
+            'imageurl' => $validatedData['imageurl'],
+            'logo_url' => $validatedData['logo_url'],
             'tmdb_id' => $validatedData['tmdb_id']
         ]);
         
